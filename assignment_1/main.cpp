77f438688d59494c01e77ecf70ffb512c16f968d
Ole-Johan Ovreas <olejohanjo@student.uia.no>
2022-01-21 12:03:20 +0100

    la til kommentara i koden

--- assignment_1/main.cpp
+++ assignment_1/main.cpp
@@ -1,22 +1,24 @@
 #include "mbed.h"
 
-DigitalOut led(PC_5);
+DigitalOut led(PC_5); //Hvilken kobling på kortet vi bruker
 
-BufferedSerial serial_port(USBTX, USBRX, 115200);
+BufferedSerial serial_port(USBTX, USBRX, 115200); //Baud rate
+
+
 
 int main(){
-    char buf[32] = {0};
+    char buf[32] = {0}; //Bufferen som skal inneholde input på mikrokontrolleren
 
-    while (true)
+    while (true) //Uendelig while loop
     {
-        if (int num = serial_port.read(buf, sizeof(buf))) {
-            if(strstr(buf, "1")){
+        if (int num = serial_port.read(buf, sizeof(buf))) { //read funksjon for å lese tastetrykk. Liten failsafe med 'int num'
+            if(strstr(buf, "1")){ //sjekker om 1 (eller 0 på neste) er substrings av det som ligger i bufferen
                 led = true;
             }
             else if(strstr(buf, "0")){
                 led = false;
             }
-            serial_port.write(buf, num);
+            serial_port.write(buf, num); //printer det som står i bufferen
         }
     }
 }
