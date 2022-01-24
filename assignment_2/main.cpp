944ee3c551a24012fa1cb3fadb4fc61395add8f0
Ole-Johan Ovreas <olejohanjo@student.uia.no>
2022-01-24 19:02:14 +0100

    testfil for assignment_2, samt endring i .mbedignore for å drastisk redusere build time på assignment_2

---
+++ assignment_2/main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+
+DigitalOut led1(PB_1);
+BufferedSerial serial_port(USBTX, USBRX, 115200);
+
+// main() runs in its own thread in the OS
+int main()
+{
+    char buf[32] = {0};
+    while (true) {
+        if (int num = serial_port.read(buf, sizeof(buf))){
+        
+        if(strstr(buf, "1")){ //sjekker om 1 (eller 0 på neste) er substrings av det som ligger i bufferen
+                led1 = true;
+            }
+            else if(strstr(buf, "0")){
+                led1 = false;
+            }
+
+        serial_port.write(buf, num);
+
+        }
+    }
+}
+
