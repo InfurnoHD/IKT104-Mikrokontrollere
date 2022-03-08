f5c565023db9680bdce87109769ad1aa8b3c6139
Ole-Johan Ovreas <olejohanjo@student.uia.no>
2022-03-08 18:42:01 +0100

    formatering

--- assignment_6/main.cpp
+++ assignment_6/main.cpp
@@ -6,14 +6,14 @@ void parseJsonData(char *chunks) {
 
   using json = nlohmann::json;
 
-//fjerner metadata fra responsen
+  // fjerner metadata fra responsen
   char *ptr = strchr(chunks, '{');
   size_t index = (chunks - ptr) + 1;
 
-//parser
+  // parser
   json j_object = json::parse(ptr, nullptr, false);
 
-//printer data som oppgaven etterspurte
+  // printer data som oppgaven etterspurte
   if (j_object.is_discarded()) {
     printf("\nThe input is invalid JSON\n");
     return;
@@ -65,11 +65,11 @@ nsapi_size_or_error_t readResponse(Socket &socket, char *buffer,
   int remaining_bytes = buffer_length;
   int received_bytes = 0, counter = 0;
 
-//lager chunk med størrelse på 100 byte
+  // lager chunk med størrelse på 100 byte
   char chunk[100] = {0};
   nsapi_size_or_error_t result = 1;
 
-//while loop som henter data om det er mer igjen
+  // while loop som henter data om det er mer igjen
   while (result > 0) {
     result = socket.recv(chunk, 100);
 
@@ -79,7 +79,7 @@ nsapi_size_or_error_t readResponse(Socket &socket, char *buffer,
         return received_bytes;
       }
 
-      //printer chunks på 100 byte og ... mellom hver print
+      // printer chunks på 100 byte og ... mellom hver print
       printf("%c", chunk[i]);
       chunks[counter] = chunk[i];
       chunk[i] = 0;
@@ -92,7 +92,7 @@ nsapi_size_or_error_t readResponse(Socket &socket, char *buffer,
 
 int main() {
 
-//henter config frå json filaog kobler til nettet
+  // henter config frå json filaog kobler til nettet
   NetworkInterface *network = NetworkInterface::get_default_instance();
 
   if (!network) {
@@ -114,7 +114,7 @@ int main() {
 
   printf("Connected to the network!\n");
 
-  //setter socket
+  // setter socket
   SocketAddress address;
   network->get_ip_address(&address);
   TCPSocket *socket = new TCPSocket;
@@ -132,7 +132,7 @@ int main() {
     socket->close();
   }
 
-//kobler til mocky.io
+  // kobler til mocky.io
   address.set_port(80);
   result = socket->connect(address);
 
@@ -143,7 +143,7 @@ int main() {
     printf("Connected to server!\n");
   }
 
-//lager og sender GET requesten
+  // lager og sender GET requesten
   const char request[] = "GET /v2/5e37e64b3100004c00d37d03 HTTP/1.1\r\n"
                          "Host: www.mocky.io\r\n"
                          "Connection: close\r\n"
@@ -156,14 +156,14 @@ int main() {
     socket->close();
   }
 
-  //definerer varibler og leser responsen
+  // definerer varibler og leser responsen
   static constexpr size_t HTTP_RESPONSE_BUF_SIZE = 100;
   static char response[HTTP_RESPONSE_BUF_SIZE];
   static char chunks[2000];
 
   result = readResponse(*socket, response, HTTP_RESPONSE_BUF_SIZE, chunks);
 
-//printer heile responsen
+  // printer heile responsen
   printf("%s", chunks);
 
   if (result != 0) {
@@ -171,6 +171,6 @@ int main() {
     socket->close();
   }
 
-//parser data og printer
+  // parser data og printer
   parseJsonData(chunks);
 }
