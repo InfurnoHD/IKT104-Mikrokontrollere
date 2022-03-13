9a6eb8852162ea25a66cfe014a28d5f9f9ab49c6
Ole-Johan Ovreas <olejohanjo@student.uia.no>
2022-03-14 00:05:12 +0100

    glømte litt kommentara

--- assignment_7/main.cpp
+++ assignment_7/main.cpp
@@ -12,15 +12,19 @@ constexpr uint32_t HTTP_REQUEST_BUFFER_SIZE = 400;
 constexpr uint32_t JSON_CONTENT_BUFFER_SIZE = 300;
 constexpr uint32_t HTTP_RESPONSE_BUFFER_SIZE = 400;
 
+// Funksjon for å sende HTTP POST
 nsapi_size_or_error_t sendData(float temp, float hum, Socket *socket,
                                char *httpRequest, char *jsonContent) {
 
+  // Variabler
   nsapi_size_t bytes_to_send = strlen(httpRequest);
   nsapi_size_or_error_t bytes_sent = 0;
 
+  // Lager jsonContent
   std::snprintf(jsonContent, JSON_CONTENT_BUFFER_SIZE,
                 "{\"temperature\": %.2f, \"humidity\": %.2f}", temp, hum);
 
+  // Lager httpRequest
   std::snprintf(httpRequest, JSON_CONTENT_BUFFER_SIZE,
                 "POST /api/v1/dJwwWNFhpTdNy8qjCZUz/telemetry HTTP/1.1\r\n"
                 "Host: 192.168.11.66:9090\r\n"
@@ -30,10 +34,12 @@ nsapi_size_or_error_t sendData(float temp, float hum, Socket *socket,
                 "\r\n",
                 strlen(jsonContent));
 
+  // Kombinerer jsonContent og httpRequest
   strcat(httpRequest, jsonContent);
 
   printf("\nSending message: \n%s\n\n", httpRequest);
 
+  // Loop for å sjekke om alt blir sendt
   while (bytes_to_send) {
 
     bytes_sent = socket->send(httpRequest + bytes_sent, bytes_to_send);
